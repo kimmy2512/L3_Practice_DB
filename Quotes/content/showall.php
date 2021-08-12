40d29b50c03d7cc1ad75fb6b4be2f44b68f236f5
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2021-08-12 21:02:08 +1200

    up to checkpoint 3

--- Quotes/content/showall.php
+++ Quotes/content/showall.php
@@ -13,60 +13,20 @@ do {
 
     $quote = preg_replace('/[^A-Za-z0-9.,\s\'\-]/', ' ', $find_rs['Quote']);
 
-    // author name...
-    $first = $find_rs['First'];
-    $middle = $find_rs['Middle'];
-    $last = $find_rs['Last'];
-
-    $full_name = $first." ".$middle." ".$last;
+    include("get_author.php");
 
     ?>
 <div class="results">
     <p>
         <?php echo $quote; ?><br />
+
+        <!-- Display author name -->
         <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID'];?>">
             <?php echo $full_name; ?>
         </a>
     </p>
 
-    <!-- Subject tags go here -->
-    <p>
-        <?php
-            $sub1_ID = $find_rs['Subject1_ID'];
-            $sub2_ID = $find_rs['Subject2_ID'];
-            $sub3_ID = $find_rs['Subject3_ID'];
-
-            $all_subjects = array($sub1_ID, $sub2_ID, $sub3_ID);
-
-            // loop through subject ID's and look up the subject name
-            foreach($all_subjects as $subject) {
-                // get subject name
-                $sub_sql = "SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
-                $sub_query = mysqli_query($dbconnect, $sub_sql);
-                $sub_rs = mysqli_fetch_assoc($sub_query);
-
-                if($subject != 0)
-                {
-
-
-                ?>
-            <-- show subjects -->
-            <span class="tag">
-                <a href="index.php?page=subject&subjectID=<?php echo $sub_rs['Subject_ID']; ?>">
-                    <?php echo $sub_rs['Subject']; ?>
-                </a>
-            </span> &nbsp;
-
-        <?php
-
-            } // end subject exists if
-
-            unset($subject);
-                
-            } // end subject loop
-
-        ?>
-    <p>
+    <?php include("show_subjects.php"); ?>
 </div>
 
 <br />
