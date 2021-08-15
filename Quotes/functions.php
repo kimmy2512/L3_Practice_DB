6e56ebd7eb56a6bebcb288ff136fc19a66a80787
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2021-08-15 19:38:59 +1200

    deleted useless code

--- Quotes/functions.php
+++ Quotes/functions.php
@@ -109,42 +109,4 @@ function get_rs($dbconnect, $sql)
     
     return $find_rs;
 }
-    
-    $all_entities = array($entity_1, $entity_2);
-    // loop through items and look up their values...
-    
-    // Counts # of countries that without ID zero...
-    $num_entities = count(array_filter($all_entities));
-    
-        
-    if ($num_entities == 1)
-    {
-    echo "<b>".$label_sg."</b>: ";
-    }
-    
-    else { echo "<b>".$label_pl."</b>: ";}
-    
-    foreach ($all_entities as $entity) {
-    
-    $entity_sql = "SELECT * FROM $table WHERE $entity_ID = $entity";
-    $entity_query = mysqli_query($dbconnect, $entity_sql);
-    $entity_rs = mysqli_fetch_assoc($entity_query);
-    
-    if ($entity != 0) {
-        
-    ?>
-    
-        
-    <span class="author_entity tag"><?php echo $entity_rs[$entity_name]; ?> </span> &nbsp;
-      
-    <?php
-        
-    } // end entity if
-  
-    // break reference with the last element as per the manual
-    unset($entity);
-        
-    }
-     
-
-?>
+   
