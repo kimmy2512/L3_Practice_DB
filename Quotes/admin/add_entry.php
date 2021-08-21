a1dea2e6a82c081d7f6ec7b36a7657e721ba1109
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2021-08-21 19:40:19 +1200

    add author part 2

--- Quotes/admin/add_entry.php
+++ Quotes/admin/add_entry.php
@@ -5,6 +5,38 @@ if (isset($_SESSION['admin'])) {
 
     $author_ID = $_SESSION['Add_Quote'];
 
+    if ($author_ID=="unknown") {
+
+    // get country & occupation lists from database
+    $all_countries_sql = "SELECT * FROM `country` ORDER BY `Country` ASC ";
+    $all_countries = autocomplete_list($dbconnect, $all_countries_sql, 'Country');
+
+    $all_occupations_sql = "SELECT * FROM `career` ORDER BY `Career` ASC ";
+    $all_occupations = autocomplete_list($dbconnect, $all_occupations_sql, 'Career');
+
+    // initialise author variables
+    $first = "";
+    $middle = "";
+    $last = "";
+    $yob = "";
+    $gender_code = "";
+    $gender = "";
+    $country_1 = "";
+    $country_2 = "";
+    $occupation_1 = "";
+    $occupation_2 = "";
+
+    // Initialise country and occupation ID's
+    $country_1_ID = $country_2_ID = $occupation_1_ID = $occupation_2_ID = 0;
+
+    // set up error fields / visibility
+    $last_error = $yob_error = $gender_error = $country_1_error = $occupation_1_error = "no-error";
+
+    $last_field = $yob_field = $gender_field = "form-ok";
+    $country_1_field = $occupation_1_field = "tag-ok";
+
+    }  // end author variable initialisation if
+
     // Get subject / topic list from database
     $all_tags_sql = "SELECT * FROM `subject` ORDER BY `Subject` ASC ";
     $all_subjects = autocomplete_list($dbconnect, $all_tags_sql, 'Subject');
@@ -22,7 +54,7 @@ if (isset($_SESSION['admin'])) {
     $has_errors = "no";
 
     // set up error fields / visibility
-    $quote_error = $tag_1_error - "no-error";
+    $quote_error = $tag_1_error = "no-error";
     $quote_field = "form-ok";
     $tag_1_field = "tag-ok";
 
@@ -54,6 +86,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($has_errors != "yes") {
 
+    // Get subject ID's via get_ID function...
+    $subjectID_1 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_1);
+    $subjectID_2 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_2);
+    $subjectID_3 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_3);
+
+    // add entry to database
+    $addentry_sql = "INSERT INTO `quotes` (`ID`, `Author_ID`, `Quote`, `Notes`, `Subject1_ID`, `Subject2_ID`, `Subject3_ID`) VALUES (NULL, '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3');";
+    $addentry_query = mysqli_query($dbconnect, $addentry_sql);
+
+    // get quote ID for next page
+    $get_quote_sql = "SELECT * FROM `quotes` WHERE `Quote` = '$quote'";
+    $get_quote_query = mysqli_query($dbconnect, $get_quote_sql);
+    $get_quote_rs = mysqli_fetch_assoc($get_quote_query);
+
+    $quote_ID = $get_quote_rs['ID'];
+    $_SESSION['Quote_Success']=$quote_ID;
+
+    // Go to success page...
+    header('Location: index.php?page=quote_success');
+
     }   // end add entry to database if
 
 }   // end submit button if
@@ -73,6 +125,100 @@ else {
 
 <form autocomplete="off" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=../admin/add_entry");?>" enctype="multipart/form-data">
 
+    <?php 
+    // fields to add new author information
+
+    if ($author_ID=="unknown") {
+
+    ?>
+
+    <!-- Author's first name, optional -->
+    <input class="add-field" type="text" name="first" value="<?php echo $first; ?>" placeholder="Author's First Name" />
+
+    <br /><br />
+
+    <input class="add-field" type="text" name="middle" value="<?php echo $middle; ?>" placeholder="Author's Middle Name (optional)" />
+
+    <br /><br />
+
+    <div class="<?php echo $last_error; ?>">
+        Author's last name can't be blank
+    </div>
+
+    <input class="add-field <?php echo $last_field; ?>" type="text" name="last" value="<?php echo $last; ?>" placeholder="Author's Last Name" />
+
+    <br /><br />
+
+    <select class="adv <?php echo $gender_field; ?>" name="gender">
+
+        <?php
+        if($gender_code=="") {
+            ?>
+                <option value="" selected>Gender (Choose something)...
+                </option>
+        <?php
+
+        }   // end gender not chose if
+
+        else {
+            ?>
+                <option value="<?php echo $gender_code;?>" selected><?php echo $gender; ?></option>
+        <?php
+
+        }   // end gender chosen else
+        ?>
+
+        <option value="F">Female</option>
+        <option value="M">Male</option>
+
+    </select>
+
+    <br /><br />
+
+    <div class="<?php echo $yob_error; ?>">
+        Author's Year of Birth can't be blank
+    </div>
+
+    <input class="add-field <?php echo $yob_field; ?>" type="next" name="yob" value="<?php echo $yob; ?>" placeholder="Author's year of birth" />
+
+    <br /><br />
+
+    <div class="<?php echo $country_1_error ?>">
+        Please enter at least one country
+    </div>
+
+    <div class="autocomplete ">
+        <input class="<?php $country_1_field; ?>" id="country1" type="next" name="country1" placeholder="Country 1 (Start Typeing)...">
+    </div>
+
+    <br /><br />
+    
+    <div class="autocomplete ">
+        <input id="country2" type="next" name="country2" placeholder="Country 2 (Start Typeing)...">
+    </div>
+
+    <br /><br />
+
+    <div class="<?php echo $occupation_1_error ?>">
+        Please enter at least one country
+    </div>
+
+    <div class="autocomplete ">
+        <input class="<?php $occupation_1_field; ?>" id="occupation1" type="next" name="occupation1" placeholder="Occupation 1 (Start Typeing)...">
+    </div>
+
+    <br /><br />
+    
+    <div class="autocomplete ">
+        <input id="occupation2" type="next" name="occupation2" placeholder="Occupation 2 (Start Typeing)...">
+    </div>
+
+    <?php
+
+    }  // end new author fields
+
+    ?>
+
 <!-- Quote text area -->
     <div class="<?php echo $quote_error; ?>">
         This field can't be blank
@@ -90,19 +236,19 @@ else {
     </div>
 
     <div class="autocomplete">
-        <input class="<?php echo $tag_1_field; ?>" id="subject1" type="text" name="Subject_1" placeholder="Subject 1 (Start typing)...">
+        <input class="<?php echo $tag_1_field; ?>" id="subject1" type="text" name="Subject_1" value="<?php echo $tag_1; ?>" placeholder="Subject 1 (Start typing)...">
     </div>
 
     <br/><br />
 
     <div class="autocomplete">
-        <input id="subject2" type="text" name="Subject_2" placeholder="Subject 2 (Start typing)...">
+        <input id="subject2" type="text" name="Subject_2" value="<?php echo $tag_2; ?>" placeholder="Subject 2 (Start typing)...">
     </div>
 
     <br/><br />
 
     <div class="autocomplete">
-        <input id="subject3" type="text" name="Subject_3" placeholder="Subject 3 (Start typing)...">
+        <input id="subject3" type="text" name="Subject_3" value="<?php echo $tag_3; ?>" placeholder="Subject 3 (Start typing)...">
     </div>
 
     <br/><br />
@@ -123,5 +269,13 @@ var all_tags = <?php print("$all_subjects"); ?>;
 autocomplete(document.getElementById("subject1"), all_tags);
 autocomplete(document.getElementById("subject2"), all_tags);
 autocomplete(document.getElementById("subject3"), all_tags);
+
+var all_countries = <?php print("$all_countries"); ?>;
+autocomplete(document.getElementById("country1"), all_countries);
+autocomplete(document.getElementById("country2"), all_countries);
+
+var all_occupations = <?php print("$all_occupations"); ?>;
+autocomplete(document.getElementById("occupation1"), all_occupations);
+autocomplete(document.getElementById("occupation2"), all_occupations);
 
 </script>
--- Quotes/admin/adminlogin.php
+++ Quotes/admin/adminlogin.php
@@ -8,16 +8,16 @@ $username = $_REQUEST['username'];
 $options = ['cost' => 9,];
 
 // Get username and hashed password from database
-$login_sql="SELECT * FROM `users` WHERE `username` = '$username'";
+$login_sql="SELECT * FROM `users` WHERE `Username` = '$username'";
 $login_query=mysqli_query($dbconnect,$login_sql);
 $login_rs = mysqli_fetch_assoc($login_query);
 
 // Hash password and compare with password in database
-if (password_verify($_REQUEST['password'] ,$login_rs['password'])) {
+if (password_verify($_REQUEST['password'] ,$login_rs['Password'])) {
 
     // password matches
     echo 'Password is valid!';
-    $_SESSION['admin']=$login_rs['username'];
+    $_SESSION['admin']=$login_rs['Username'];
     header("Location: index.php?page=../admin/admin_panel");
 
 }    // end valid password if
@@ -27,7 +27,7 @@ else {
     echo 'Invalid password.';
     unset($_SESSION);
     $login_error = "Incorrect username / password";
-    header("Location: index.php?page../admin/login&error=$login_error");
+    header("Location: index.php?page=../admin/login&error=$login_error");
 
 }    // end invalid password else
 
